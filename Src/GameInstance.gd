843c54417d3e4458ead5c26cec592c89e205ee82
themangomago <markus.schneider84@gmail.com>
2021-02-15 12:03:45 +0100

    basic UI setup

--- Src/GameInstance.gd
+++ Src/GameInstance.gd
@@ -1,5 +1,63 @@
 extends Control
 
+# This will be used for save/load game
+var gameState = {
+	resources = { water = 20, food = 41, faith = 80 }
+	
+}
+
+
+###############################################################################
+# Functions
+###############################################################################
+
+func _ready():
+	initDebug()
+
+func initDebug():
+	var cat = Debug.addCategory("Resources")
+	Debug.addOption(cat, "Add Water", funcref(self, "debugAddResource"), Types.ResourceType.Water)
+	Debug.addOption(cat, "Add Food", funcref(self, "debugAddResource"), Types.ResourceType.Food)
+	Debug.addOption(cat, "Add Faith", funcref(self, "debugAddResource"), Types.ResourceType.Faith)
+	Debug.addOption(cat, "Clear All", funcref(self, "debugClearResource"), null)
+
+func debugAddResource(type): #Types.ResourceType
+	match type:
+		Types.ResourceType.Water:
+			gameState.resources.water += 100
+			updateResourceGui(type, gameState.resources.water)
+		Types.ResourceType.Food:
+			gameState.resources.food += 100
+			updateResourceGui(type, gameState.resources.food)
+		_:
+			gameState.resources.faith += 100
+			updateResourceGui(type, gameState.resources.faith)
+	
+func debugClearResource(_d):
+	gameState.resources.water = 0
+	gameState.resources.food = 0
+	gameState.resources.faith = 0
+	updateResourceGui(Types.ResourceType.Water, 0)
+	updateResourceGui(Types.ResourceType.Food, 0)
+	updateResourceGui(Types.ResourceType.Faith, 0)
+
+
+func updateResourceGui(resourceType, val): #Types.ResourceType
+	match resourceType:
+		Types.ResourceType.Water:
+			$TopBar/Water/Label.set_text(str(val))
+		Types.ResourceType.Food:
+			$TopBar/Food/Label.set_text(str(val))
+		Types.ResourceType.Faith:
+			$TopBar/Faith/Label.set_text(str(val))
+		_:
+			print("GameInstance.gd: Invalid Resource Type")
+
+
+
+###############################################################################
+# Callbacks
+###############################################################################
 
 func _on_ButtonBack_button_up():
 	Events.emit_signal("play_sound", "menu_click")
@@ -14,3 +72,15 @@ func _on_ButtonMusic_button_up():
 func _on_ButtonSound_button_up():
 	Events.emit_signal("play_sound", "menu_click")
 	Events.emit_signal("switch_sound", !Global.userConfig.sound)
+
+
+func _on_ExpButton_button_up():
+	pass # Replace with function body.
+
+
+func _on_MoveButton_button_up():
+	pass # Replace with function body.
+
+
+func _on_TurnButton_button_up():
+	pass # Replace with function body.
