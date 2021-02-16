421515936b38d7b7ed4d9e899e9799f9237c14d6
Zach777 <MeteorCity3109@protonmail.com>
2021-02-16 09:20:20 -0500

    Grid has temporary color pattern

--- Src/Menu/InGame/Gathering/GatheringSpot.gd
+++ Src/Menu/InGame/Gathering/GatheringSpot.gd
@@ -3,10 +3,26 @@ class_name GatheringSpot
 
 var location_in_grid : Vector2 = Vector2(-1,-1) setget set_location, get_location
 
-func _init(location : Vector2 = Vector2(-1,-1)) :
+#This will determine spot type and thus what texture we use.
+var spot_type : int = -1 setget set_spot_type, get_spot_type
+
+func _init(location : Vector2 = Vector2(-1,-1), new_spot_type : int = -1) :
 	location_in_grid = location
+	spot_type = new_spot_type
+	#Temporary modulation to show what type we are.
+	if spot_type == 0 : #LAKE
+		modulate = Color(0, 0.1, 0.5)
+	elif spot_type == 1 : #HUNTING
+		modulate = Color(1,0,0)
+	elif spot_type == 2 : #RUINS
+		modulate = Color(0.4,0.4,0.4)
+	elif spot_type == 3 : #BERRIES
+		modulate = Color(0,1,0)
+	elif spot_type == 4 : #Creek :
+		modulate = Color( 0,0,1)
 	
-	#Make sure values of the location are not incorrect.
+	
+	#Make sure values of the location are correct.
 	assert(location_in_grid.x >= 0 && location_in_grid.y >= 0)
 
 #If the player presses me, add one point to the path.
@@ -17,10 +33,16 @@ func _pressed() -> void :
 	Events.emit_signal(Events.GATHER_POINT_ADDED, location_in_grid)
 	
 	#Let the player know where they clicked.
-	print("You have clicked [%s] on the Expedition" % [str(location_in_grid)] )
+	print("You have clicked [%s] on the Expedition. Spot value of %s" % [str(location_in_grid), str(spot_type)] )
 
 func get_location() -> Vector2 :
 	return location_in_grid
 
 func set_location(new_location : Vector2) -> void :
 	location_in_grid = new_location
+
+func get_spot_type() -> int :
+	return spot_type
+
+func set_spot_type(new_spot_type : int) -> void :
+	spot_type = new_spot_type
--- Src/Menu/InGame/Gathering/GridContainer.gd
+++ Src/Menu/InGame/Gathering/GridContainer.gd
@@ -15,10 +15,13 @@ func new_location(location : Array) -> void :
 	for x in location :
 		var vcontainer : VBoxContainer = VBoxContainer.new()
 		vcontainer.set("custom_constants/separation", 0)
+		vcontainer.grow_horizontal = Control.GROW_DIRECTION_BOTH
+		vcontainer.grow_vertical = Control.GROW_DIRECTION_BOTH
+		vcontainer.alignment = BoxContainer.ALIGN_CENTER
 		call_deferred("add_child", vcontainer)
 		var y_int : int = 0
 		for y in x :
-			var spot : GatheringSpot = GatheringSpot.new(Vector2(x_int, y_int))
+			var spot : GatheringSpot = GatheringSpot.new(Vector2(x_int, y_int), y)
 			vcontainer.call_deferred("add_child", spot)
 			y_int += 1
 		x_int += 1
