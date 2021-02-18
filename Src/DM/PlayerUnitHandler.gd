c3751b539c0a0c411a0cf804d479da1ed65ad13f
Zach777 <MeteorCity3109@protonmail.com>
2021-02-18 08:25:21 -0500

    Reset temp slotted units to 1 after expedition

--- Src/DM/PlayerUnitHandler.gd
+++ Src/DM/PlayerUnitHandler.gd
@@ -31,6 +31,8 @@ func add_unit() -> void :
 #Move units from temp_slotting to slotted
 func confirm_slotting() -> void :
 	set_available_units(available_units - temp_slotted_units)
+	yield(get_tree().create_timer(0.1), "timeout")
+	temp_slot_units(1)
 
 #Return number of units not away from camp. Either available or sleeping.
 func get_available_units() -> int :
