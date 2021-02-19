bd5449402db2996e7beaa4f6b73a0e0d361b7d84
mago <markus.schneider84@gmail.com>
2021-02-19 12:58:31 +0100

    cleaning up expeditionwindow

--- Src/ExpeditionWindow/Content.gd
+++ Src/ExpeditionWindow/Content.gd
@@ -2,11 +2,6 @@ extends Control
 
 
 func _ready():
-	for i in range(17):
-		$CheckList.addOption("Option " + str(i))
-	$CheckList.displayPage(0)
-	updateButtons()
-	
 	#warning-ignore:return_value_discarded
 	$BaseButtonGreen.connect("pressed", Events, "emit_signal", [Events.EXPEDITION_STARTED])
 	#warning-ignore:return_value_discarded
@@ -18,29 +13,8 @@ func reset():
 func setup(payload):
 	print("ExpeditionWindow setup()")
 
-func updateButtons():
-	var itemsPerPage = $CheckList.optionsPerPage
-	var itemsTotal = $CheckList.optionCount
-	var page = $CheckList.activePage
-	
-	if page == 0:
-		$DownButton.disabled = true
-	else:
-		$DownButton.disabled = false
 
-	if page < (itemsTotal - 1)/itemsPerPage :
-		$UpButton.disabled = false
-	else:
-		$UpButton.disabled = true
-	
 
-func _on_UpButton_button_up():
-	$CheckList.displayPage($CheckList.activePage + 1)
-	updateButtons()
-
-func _on_DownButton_button_up():
-	$CheckList.displayPage($CheckList.activePage - 1)
-	updateButtons()
 
 func _on_BaseButtonPink_button_up():
 	print("button")
