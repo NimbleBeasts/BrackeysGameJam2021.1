4f3ae367139e9e9496eecf16342099d1a792ed15
mago <markus.schneider84@gmail.com>
2021-02-20 20:33:03 +0100

    fixed by chance and unittype

--- Src/Unit/UnitClass.gd
+++ Src/Unit/UnitClass.gd
@@ -3,24 +3,28 @@ class_name Unit
 
 
 var uid =randi()
-var type : String
+var type : int
 var available : bool = true
 
 
 func set_by_name(type_name : String) -> void :
 	for entry in Data.units:
 		if entry.type == type_name:
-				type = type_name
+				type = Types.UnitTypes.keys().find(entry.type.capitalize())
 				name = entry.names[randi() % entry.names.size()]
 				available = true
 		
 	uid = str(self).sha256_text()
 
 func set_by_chance() -> void :
-	var entry : Dictionary = Data.units[(randi() % Data.units.size() - 2) + 1]
+	var rand = randi() % 100
+	var accumulatedChance = 0
 	
-	type = entry.type
-	name = entry.names[randi() % entry.names.size()]
-	available = true
-	
+	for entry in Data.units:
+		accumulatedChance += entry.chance
+		if accumulatedChance > rand:
+			type = Types.UnitTypes.keys().find(entry.type.capitalize())
+			name = entry.names[randi() % entry.names.size()]
+			available = true
+
 	uid = str(self).sha256_text()
