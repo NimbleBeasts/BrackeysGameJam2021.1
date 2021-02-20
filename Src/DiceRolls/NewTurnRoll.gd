8d63731135b697b24b1c146af8aefe0b66579342
Zach777 <MeteorCity3109@protonmail.com>
2021-02-20 18:44:35 -0500

    Events and expedition returns work

--- Src/DiceRolls/NewTurnRoll.gd
+++ Src/DiceRolls/NewTurnRoll.gd
@@ -9,7 +9,7 @@ func roll_chances() -> int :
 	var possible_events : Array = Data.events["turnRandom"]
 	
 	#2 out of 5 nothing happens.
-	var chances : int = (randi() % (possible_events.size() + 6)) - 6
+	var chances : int = (randi() % (possible_events.size() + 4)) - 4
 	if chances <= 0 :
 		return 1
 	Events.emit_signal("window_show", Types.WindowType.Event, {"eventType": Types.EventTypes.TurnRandom, "id":  chances})
